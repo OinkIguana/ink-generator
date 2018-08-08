7465d7c169005f9d02fc8f7c6ffd0d09f83dabf5
Cameron Eldridge <cameldridge+git@gmail.com>
2018-08-07 21:28:03 -0400

    Add a space where the glue goes

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -13,7 +13,7 @@ impl Part {
     fn to_string(&self) -> String {
         match self {
             Part::Text(string) => string.to_owned(),
-            Part::Glue => "".to_owned(),
+            Part::Glue => " ".to_owned(),
         }
     }
 }
